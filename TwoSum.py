768741dc89b06308e63919bd61c0719ff17b5638
Rajkumar Patel <rajpatel9498@gmail.com>
2020-09-07 23:34:36 -0700

    Update TwoSum.py

--- TwoSum.py
+++ TwoSum.py
@@ -13,3 +13,13 @@ class Solution:
                 return (dictonary[x],i)
             else:
                 dictonary[nums[i]] = i
+                
+# Solution - 2 Using For loop
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        l = len(nums)
+        for i in range(l):
+            for j in range(i+1,l):
+                if (nums[j] + nums[i]) == target:
+                    return i,j
