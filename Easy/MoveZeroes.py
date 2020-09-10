a9cced05539f75f7fbccb81392688f48526ab028
Rajkumar Patel <rajpatel9498@gmail.com>
2020-09-10 15:15:54 -0700

    Create MoveZeroes.py

---
+++ Easy/MoveZeroes.py
@@ -0,0 +1,11 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        
+        
+        for i in nums:
+            if i==0:
+                nums.pop(nums.index(i)) 
+                nums.append(0)
